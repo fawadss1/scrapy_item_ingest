55665c7375648d5aa1e8dcaf7810596b1e083c80
Fawad <fawadstar6@gmial.com>
2025-11-21 12:21:10 +0500

    removed unused code

--- scrapy_item_ingest/utils/serialization.py
+++ scrapy_item_ingest/utils/serialization.py
@@ -2,25 +2,6 @@
 Serialization utilities for converting data to JSON-serializable format.
 """
 import json
-from datetime import datetime, date, time
-from decimal import Decimal
-
-
-def serialize_stats(obj):
-    """Recursively convert stats to JSON-serializable format"""
-    if isinstance(obj, dict):
-        return {key: serialize_stats(value) for key, value in obj.items()}
-    elif isinstance(obj, (list, tuple)):
-        return [serialize_stats(item) for item in obj]
-    elif isinstance(obj, (datetime, date, time)):
-        return obj.isoformat()
-    elif isinstance(obj, Decimal):
-        return float(obj)
-    elif isinstance(obj, (int, float, str, bool)) or obj is None:
-        return obj
-    else:
-        # For any other type, convert to string
-        return str(obj)
 
 
 def serialize_item_data(item_dict):
